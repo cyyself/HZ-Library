a7872d596471b6fb88b4da16370f0721a439e9dc
Yangyu Chen <cyy@cyyself.name>
2021-02-11 17:56:19 +0800

    finished decode

---
+++ hzlib.php
@@ -0,0 +1,27 @@
+<?php
+function hz_decode($s) {
+	$len = strlen($s);
+	$buf = '';
+	$hz_mode = 0;
+	for ($i=0;$i<$len;$i++) {
+		if ($s[$i] == '~') {
+			if ($i + 1 < $len) {
+				switch ($s[$i+1]) {
+					case '~':
+						$buf += '~';
+						break;
+					case '{':
+						$hz_mode = 128;
+						break;
+					case '}':
+						$hz_mode = 0;
+						break;
+					default:
+				}
+				$i += 1;
+			}
+		}
+		else $buf .= chr(ord($s[$i]) ^ $hz_mode);
+	}
+	return iconv('gb2312','utf-8',$buf);
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# HZ-Library
+
+A library for implement [RFC 1843](https://tools.ietf.org/html/rfc1843) written in php.
